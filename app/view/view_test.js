8081b5da44e28c897e260c7fa35b6cbe2b2e14e2
iliya_zanev <iliya.zanev@bluebridgeltd.com>
2017-11-04 15:45:43 +0000

    Tests for 10000 and 20000 prime numbers

--- app/view/view_test.js
+++ app/view/view_test.js
@@ -1,16 +1,28 @@
 'use strict';
 
-describe('myApp.view1 module', function() {
+describe('primesApp.view module', function() {
+    var $scope;
 
-  beforeEach(module('myApp.view1'));
+    beforeEach(module('primesApp.view'));
 
-  describe('view1 controller', function(){
-
-    it('should ....', inject(function($controller) {
-      //spec body
-      var view1Ctrl = $controller('View1Ctrl');
-      expect(view1Ctrl).toBeDefined();
+    beforeEach(inject(function($rootScope, $controller) {
+        $scope = $rootScope.$new();
+        $controller('ViewCtrl', {
+            $scope: $scope
+        });
     }));
 
-  });
+    describe('gets prime numbers', function(){
+
+        it('calculates 10000 primes', function() {
+            var primes = $scope.getPrimes(10000);
+            expect(primes[9999]).toBe(104729);
+        });
+
+        it('calculates 20000 primes', function() {
+            var primes = $scope.getPrimes(20000);
+            expect(primes[19999]).toBe(224737);
+        });
+
+    });
 });
