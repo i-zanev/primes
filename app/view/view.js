5c2e375d02c14ed0be2a0092d8740d1af1c08ced
iliya_zanev <iliya.zanev@bluebridgeltd.com>
2017-11-04 15:34:01 +0000

    Show me some primes

--- app/view/view.js
+++ app/view/view.js
@@ -10,12 +10,46 @@ angular.module('primesApp.view', ['ngRoute'])
     }])
 
     .controller('ViewCtrl', ['$scope', function($scope) {
-      $scope.n = 0;
       $scope.model = {
-          firstNPrimes: []
-          };
+        n: 0,
+        firstNPrimes: []
+      };
 
       $scope.getPrimes = function(n) {
-        $scope.model.firstNPrimes = n;
+
+        //the first prime number is 2
+        var firstNPrimes = [2];
+
+        //if more than 1 prime number is requested
+        if(n > 1) {
+          //the second prime number is 3
+          firstNPrimes.push(3);
+          //skip 4 as divisible by 2 and set the first number to check as 5
+          var primeNumber = 5;
+
+          while (firstNPrimes.length < n) {
+            var innerCounter = 0;
+            var isPrime = true;
+
+            //check if number is prime
+            while(firstNPrimes[innerCounter] <= Math.sqrt(primeNumber)) {
+              if(primeNumber % firstNPrimes[innerCounter] === 0) {
+                isPrime = false;
+                break;
+              }
+
+              innerCounter++;
+            }
+
+            if(isPrime)	{
+              firstNPrimes.push(primeNumber);
+            }
+            //omit even numbers as these are always divisible by 2
+            primeNumber += 2;
+          }
+        }
+
+        $scope.model.firstNPrimes = firstNPrimes;
+        return firstNPrimes;
       };
     }]);
